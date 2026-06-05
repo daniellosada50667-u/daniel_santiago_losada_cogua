ae92339fa0f720b6975fa01adb68ca8ed7b746ec
YanethMejiaRendon <YanethM@users.noreply.github.com>
2026-06-05 07:36:45 -0500

    Delete src/App.java

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Automovil miAuto = new Automovil();
-        miAuto.ImprimirInformacionVehiculo("Mercedes");
-        miAuto.ImprimirInformacionVehiculoV2();
-    }
-}
